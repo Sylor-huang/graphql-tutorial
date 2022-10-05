f170758ab464901ce3264df88112bfa53cb741e7
Sylor-huang <sylor_huang@126.com>
2022-10-05 16:48:17 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
+  post "/", to: "graphql#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
